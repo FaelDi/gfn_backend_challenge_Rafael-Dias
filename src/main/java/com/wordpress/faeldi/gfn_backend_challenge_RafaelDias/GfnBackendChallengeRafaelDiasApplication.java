9622db2c271c8916885717fa918db746a481ea81
faeldii@icloud.com <faeldi@Rafaels-MacBook-Pro.local>
2020-02-25 09:06:04 -0300

    Projeto Base gerado no https://start.spring.io/ com as dependencias Spring Web / Spring Batch / Spring Data JPA / Lombok / MySQL Driver

---
+++ src/main/java/com/wordpress/faeldi/gfn_backend_challenge_RafaelDias/GfnBackendChallengeRafaelDiasApplication.java
@@ -0,0 +1,13 @@
+package com.wordpress.faeldi.gfn_backend_challenge_RafaelDias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfnBackendChallengeRafaelDiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfnBackendChallengeRafaelDiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wordpress/faeldi/gfn_backend_challenge_RafaelDias/GfnBackendChallengeRafaelDiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordpress.faeldi.gfn_backend_challenge_RafaelDias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfnBackendChallengeRafaelDiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
